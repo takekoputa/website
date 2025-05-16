94509fffd036bfd254a06779092d47b051100eb9
Hoa Nguyen <hn@hnpl.org>
2025-05-16 12:28:27 -0700

    Update website
    Signed-off-by: Hoa Nguyen <hn@hnpl.org>

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,7 @@
 cp ../docs/CNAME CNAME.bak
 rm -rf ../docs/
 mkdir ../docs/
+mkdir ../docs/work/
 mv CNAME.bak ../docs/CNAME
 cp -r css ../docs/
-pandoc -s -c css/main.css -c css/about.css -B pages/header.html -A pages/footer.html pages/index.md --template templates/index.yaml -o ../docs/index.html
+pandoc -s -c css/main.css -c css/about.css -B pages/header.html -A pages/footer.html pages/work.md --template templates/work.yaml -o ../docs/work/index.html
