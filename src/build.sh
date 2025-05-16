14ee8857965047474e95400e8acbe2aef6900cc1
Hoa Nguyen <hn@hnpl.org>
2025-05-16 12:32:27 -0700

    Update CSS paths
    Signed-off-by: Hoa Nguyen <hn@hnpl.org>

--- src/build.sh
+++ src/build.sh
@@ -4,4 +4,4 @@ mkdir ../docs/
 mkdir ../docs/work/
 mv CNAME.bak ../docs/CNAME
 cp -r css ../docs/
-pandoc -s -c css/main.css -c css/about.css -B pages/header.html -A pages/footer.html pages/work.md --template templates/work.yaml -o ../docs/work/index.html
+pandoc -s -c ../css/main.css -c ../css/about.css -B pages/header.html -A pages/footer.html pages/work.md --template templates/work.yaml -o ../docs/work/index.html
